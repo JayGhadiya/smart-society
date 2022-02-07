9b278bbfaa1c91dd5500f4e18a512e02ddb21653
rahulkacha <rahulbkacha@gmail.com>
2022-02-07 17:08:53 +0530

    master admin page is working as expected and is printing the form data on submission to the console.

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
 const express = require("express");
 const ejs = require("ejs");
+const bodyParser = require("body-parser");
 
 const app = express();
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", function (req, res) {
+  res.render("home", {});
+});
+
+app.get("/master/admin/add-society", function (req, res) {
   res.render("master-admin-add-society", {});
+});
+
+app.post("/master/admin/add-society", function (req, res) {
+  console.log(req.body);
 });
 
 app.listen(5000, function () {
