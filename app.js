4ab28f2348998ee9ca1c9288748b7c34c11df6c6
rahulkacha <rahulbkacha@gmail.com>
2022-02-14 23:23:33 +0530

    implemented multer to store the file posted in amenities-form

--- app.js
+++ app.js
@@ -1,7 +1,39 @@
 const express = require("express");
 const ejs = require("ejs");
 const bodyParser = require("body-parser");
+const multer = require("multer");
 
+// SETTING UP FILENAME AND DESTINATION
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "uploads/");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+
+const fileFilter = (req, file, cb) => {
+  // rejects a file based on the file type "mimetype"
+  if (
+    file.mimetype === "image/jpeg" ||
+    file.mimetype === "image/png" ||
+    file.mimetype === "application/pdf"
+  ) {
+    cb(null, true);
+  } else {
+    cb(
+      null,
+      console.error('worng file format! you uploaded "' + file.mimetype + '"'),
+      false
+    );
+  }
+};
+const upload = multer({
+  storage: storage,
+  fileFilter: fileFilter,
+  limits: { fileSize: 1024 * 1024 * 5 }, //5 megabyte
+});
 const app = express();
 
 app.set("view engine", "ejs");
@@ -67,7 +99,7 @@ app
     res.render("admin-meetings");
   })
   .post(function (req, res) {
-    console.log(req.body);
+    console.log(req.file.mimetype);
     res.redirect("/admin/meetings");
   });
 
@@ -83,17 +115,16 @@ app
     res.redirect("/admin/circulars");
   });
 
-  // AMENITIES
+// AMENITIES
 app
-.route("/admin/amenities")
+  .route("/admin/amenities")
 
-.get(function (req, res) {
-  res.render("admin-amenities");
-})
-.post(function (req, res) {
-  console.log(req.body);
-  res.redirect("/admin/amenities");
-});
+  .get(function (req, res) {
+    res.render("admin-amenities");
+  })
+  .post(upload.single("image"), function (req, res) {
+    res.redirect("/admin/amenities");
+  });
 
 app.listen(5000, function () {
   console.log("Server started on port 5000.");
