a656f9aae7858f74ac642e985ae49fc692e0bbd6
rahulkacha <rahulbkacha@gmail.com>
2022-02-14 18:15:43 +0530

    added some new pages and routes for handle users, meetings, circulars, amenities. and did some refactoring for the routes

--- app.js
+++ app.js
@@ -14,37 +14,85 @@ app.get("/", function (req, res) {
 });
 
 // ADD A SOCIETY
-app.get("/master/admin/add-society", function (req, res) {
-  res.render("add-society", {});
-  res.redirect("/");
-});
+app
+  .route("/master/admin/add-society")
 
-app.post("/master/admin/add-society", function (req, res) {
-  res.redirect("/master/admin/add-society");
-});
+  .get(function (req, res) {
+    res.render("add-society", {});
+    res.redirect("/");
+  })
+  .post(function (req, res) {
+    console.log(req.body);
+    res.redirect("/master/admin/add-society");
+  });
 
 // MANAGE SOCITIES
-app.get("/master/admin/manage-societies", function (req, res) {
-  res.render("manage-societies");
-});
+app
+  .route("/master/admin/manage-societies")
 
-app.post("/master/admin/manage-societies", function (req, res) {
-  // deltetion of society from the database
-});
+  .get(function (req, res) {
+    res.render("manage-societies");
+  })
+  .post(function (req, res) {
+    // deltetion of society from the database
+  });
 
 // ADMIN LOGIN
-app.get("/admin/login", function (req, res) {
-  res.render("admin-login");
-});
+app
+  .route("/admin/login")
 
-app.post("/admin/login", function (req, res) {
-  console.log(req.body);
-  res.redirect("/admin/profile");
-});
+  .get(function (req, res) {
+    res.render("admin-login");
+  })
+  .post(function (req, res) {
+    console.log(req.body);
+    res.redirect("/admin/profile");
+  });
 
 // ADMIN PROFILE
 app.get("/admin/profile", function (req, res) {
   res.render("admin-profile");
+});
+
+// HANDLE USERS
+app.get("/admin/handle-users", function (req, res) {
+  res.render("admin-handle-users");
+});
+
+// MEETINGS
+app
+  .route("/admin/meetings")
+
+  .get(function (req, res) {
+    res.render("admin-meetings");
+  })
+  .post(function (req, res) {
+    console.log(req.body);
+    res.redirect("/admin/meetings");
+  });
+
+// CIRCULARS
+app
+  .route("/admin/circulars")
+
+  .get(function (req, res) {
+    res.render("admin-circulars");
+  })
+  .post(function (req, res) {
+    console.log(req.body);
+    res.redirect("/admin/circulars");
+  });
+
+  // AMENITIES
+app
+.route("/admin/amenities")
+
+.get(function (req, res) {
+  res.render("admin-amenities");
+})
+.post(function (req, res) {
+  console.log(req.body);
+  res.redirect("/admin/amenities");
 });
 
 app.listen(5000, function () {
