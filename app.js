d9c7bf43f404f50ca2e1d4f274b7de0f2b388052
rahulkacha <rahulbkacha@gmail.com>
2022-02-14 13:11:01 +0530

    added new routes of login, profile, add society, manage societies. made basic frontend templates for all the routes.

--- app.js
+++ app.js
@@ -8,20 +8,45 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// HOME PAGE
 app.get("/", function (req, res) {
   res.render("home", {});
 });
 
+// ADD A SOCIETY
 app.get("/master/admin/add-society", function (req, res) {
-  // res.render("master-admin-add-society", {});
-  res.render("test", {});
+  res.render("add-society", {});
+  res.redirect("/");
 });
 
 app.post("/master/admin/add-society", function (req, res) {
-  // console.log(req.body);
   res.redirect("/master/admin/add-society");
 });
 
+// MANAGE SOCITIES
+app.get("/master/admin/manage-societies", function (req, res) {
+  res.render("manage-societies");
+});
+
+app.post("/master/admin/manage-societies", function (req, res) {
+  // deltetion of society from the database
+});
+
+// ADMIN LOGIN
+app.get("/admin/login", function (req, res) {
+  res.render("admin-login");
+});
+
+app.post("/admin/login", function (req, res) {
+  console.log(req.body);
+  res.redirect("/admin/profile");
+});
+
+// ADMIN PROFILE
+app.get("/admin/profile", function (req, res) {
+  res.render("admin-profile");
+});
+
 app.listen(5000, function () {
-  console.log("Server started on port 5000 \nAND TEST.EJS IS BEING RENDERED.");
+  console.log("Server started on port 5000.");
 });
